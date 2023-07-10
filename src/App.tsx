dc4827deb2ceb17b52e90a84799c9151e5df27dd
mannies <priorsolution@Priors-MacBook-Pro.local>
2023-07-10 14:17:50 +0700

    add: styled-component, edit: app to hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { styled } from "styled-components";
 
-function App() {
+const Container = styled.div`
+  padding: 16px;
+  background-color: lightgreen;
+`;
+
+const H1Styled = styled.h1`
+  color: red;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <H1Styled>Hello World</H1Styled>
+    </Container>
   );
-}
+};
 
 export default App;
