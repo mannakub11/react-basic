ee839786ce83f7431730c99c96a67643f3a91a16
mannies <priorsolution@Priors-MacBook-Pro.local>
2023-07-10 15:51:07 +0700

    add: custom styled

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import { styled } from "styled-components";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Button, Typography } from "@mui/material";
+
+const Theme = createTheme({
+  typography: {
+    fontFamily: "Kanit, Inter",
+  },
+});
+
 const Container = styled.div`
   padding: 16px;
   background-color: lightgreen;
+
+  & .MuiButtonBase-root {
+    background-color: white;
+    width: 300px;
+    height: 50px;
+    border-radius: 15px;
+  }
 `;
 
 const H1Styled = styled.h1`
@@ -14,9 +30,14 @@ const H1Styled = styled.h1`
 
 const App = () => {
   return (
-    <Container>
-      <H1Styled>Hello World</H1Styled>
-    </Container>
+    <ThemeProvider theme={Theme}>
+      <Container>
+        <Typography variant="h1" color={"red"}>
+          Hello World
+        </Typography>
+        <Button> Click me</Button>
+      </Container>
+    </ThemeProvider>
   );
 };
 
